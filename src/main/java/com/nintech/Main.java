699fcd35fe8a43e267448ce0d6e7ab3e5d1a84d4
NeftaliGC <aliguiacruz@outlook.com>
2023-11-19 19:28:21 -0600

    Pantallas de Inicio y registro completas, next 'Conectar excel'

--- src/main/java/com/nintech/Main.java
+++ src/main/java/com/nintech/Main.java
@@ -1,6 +1,18 @@
 package com.nintech;
+import com.nintech.View.Pantalla_Inicio;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame("WhereEat");
+        frame.setContentPane(new Pantalla_Inicio().getP_inicio());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
     }
 }
--- src/main/java/com/nintech/View/Pantalla_Inicio.java
+++ src/main/java/com/nintech/View/Pantalla_Inicio.java
@@ -1,25 +1,106 @@
 package com.nintech.View;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Pantalla_Inicio {
-    private JPanel panel1;
+public class Pantalla_Inicio extends JPanel{
+    private JPanel p_inicio;
     private JLabel wherEatLabel;
     private JButton iniciarSesionButton;
+    private JPanel Botones;
     private JButton registrarseButton;
+    private JPanel InicioSesion;
+    private JLabel correoLabelInicio;
+    private JTextField correoFielInicio;
+    private JPasswordField passwordFielInicio;
+    private JLabel contraseñaLabelInicio;
+    private JButton IniciarSesionButton;
+    private JPanel Registrarse;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JPasswordField passwordField2;
+    private JPanel centro;
+    private JButton regresarButton;
+    private JButton regresarButtonRegistro;
+    private JButton registrarseButtonForm;
+
+    private CardLayout cardLayout = new CardLayout();
 
     public Pantalla_Inicio() {
+
+        centro.setLayout(cardLayout);
+        centro.add(getBotones(), "Card1");
+        centro.add(getInicioSesion(), "Card2");
+        centro.add(getRegistrarse(), "Card3");
+
+
+
+
         iniciarSesionButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                System.out.println("Iniciar Sesion");
+                cardLayout.show(getCentro(), "Card2");
+                centro.revalidate();
+                centro.repaint();
             }
         });
+        regresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Regresar");
+                cardLayout.show(getCentro(), "Card1");
+                centro.revalidate();
+                centro.repaint();
+            }
+        });
+        registrarseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Registrarse");
+                cardLayout.show(getCentro(), "Card3");
+                centro.revalidate();
+                centro.repaint();
+            }
+        });
+        regresarButtonRegistro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Regresar");
+                cardLayout.show(getCentro(), "Card1");
+                centro.revalidate();
+                centro.repaint();
+            }
+        });
+    }
+
+    public JPanel getP_inicio() {
+        return p_inicio;
+    }
+
+    public JPanel getCentro() {
+        return centro;
+    }
+
+    public JPanel getInicioSesion() {
+        return InicioSesion;
+    }
+
+    public JPanel getRegistrarse() {
+        return Registrarse;
+    }
+
+    public JPanel getBotones() {
+        return Botones;
     }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
     }
+
 }
+
+
